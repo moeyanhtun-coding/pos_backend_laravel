b672b35341faaf71e118b7478c8ddbc721f1bb52
AungPyaePhyo-Dev <aungpyaephyoe.dev@gmail.com>
2024-04-07 10:58:08 +0630

    Added ProductCategoryResource

--- app/Http/Resources/ProductCategoryResource.php
+++ app/Http/Resources/ProductCategoryResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductCategoryRource extends JsonResource
+class ProductCategoryResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -17,7 +17,7 @@ class ProductCategoryRource extends JsonResource
         return [
             'ProductCategoryId' => $this->ProductCategoryId,
             'ProductCategoryCode' => $this->ProductCategoryCode,
-            'ProductName' => $this->ProductName
+            'ProductCategoryName' => $this->ProductCategoryName
         ];
     }
 }
